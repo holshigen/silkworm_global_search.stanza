387d9dece5408ee1c733060b21568094355cda8c
myaguchi <myaguchi@nig.ac.jp>
2022-01-13 10:58:11 +0900

    dataTablesのスタイル修正

--- stanzas/silkworm_global_search/index.js
+++ stanzas/silkworm_global_search/index.js
@@ -4,7 +4,7 @@ import { unwrapValueFromBinding } from 'togostanza/utils';
  * jQueryはウェブアプリケーション側のPrimefacesと衝突するため通常はコメントアウトしておく。
  * Stanza単体で動作させる場合はコメントを外す。
  */
-//import * as jquery from 'https://rcshige3.nig.ac.jp/rdf/js/jquery-3.5.1.min.js';
+import * as jquery from 'https://rcshige3.nig.ac.jp/rdf/js/jquery-3.5.1.min.js';
 import * as dataTables from 'https://rcshige3.nig.ac.jp/rdf/js/jquery.dataTables.min.js';
 
 
@@ -53,14 +53,16 @@ export default class SilkwormPhenotypeSearch extends Stanza {
 
             // 結果テーブルにページャーを付ける
             $(this.root.querySelector('#resultTable')).dataTable({
-            //    "aLengthMenu" : [ 10, 25, 50, 100 ], // 表示件数の選択肢
-            //    "iDisplayLength" : 10, // 表示件数のデフォルトの値
+                // "aLengthMenu" : [ 10, 25, 50, 100 ], // 表示件数の選択肢
+                // "iDisplayLength" : 10, // 表示件数のデフォルトの値
                 "ordering" : true, // ソート
-                "searching" : true, // 検索
+                "searching" : false, // 検索
                 "oLanguage" : { // 表示される文字
-                    "sEmptyTable" : "No data found.",
-                    "sZeroRecords" : "No data found.",
-                }
+                  "sEmptyTable" : "No data found.",
+                  "sZeroRecords" : "No data found.",
+                },
+                "info": true,
+                "dom": '<"top"<"pagination"p>><"information"i><"length_changing"l>rt<"bottom"<"pagination"p>><"clear">',
             });
 
             // URI表示チェックボックス
