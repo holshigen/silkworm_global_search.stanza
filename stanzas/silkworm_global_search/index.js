84ebe55ec8e19e06f27e6c2ed87829bf85fe5177
nbrp <35756292+nbrp@users.noreply.github.com>
2022-03-09 16:43:40 +0900

    stanza単体検証

--- stanzas/silkworm_global_search/index.js
+++ stanzas/silkworm_global_search/index.js
@@ -4,7 +4,7 @@ import { unwrapValueFromBinding } from 'togostanza/utils';
  * jQueryはウェブアプリケーション側のPrimefacesと衝突するため通常はコメントアウトしておく。
  * Stanza単体で動作させる場合はコメントを外す。
  */
-//import * as jquery from 'https://rcshige3.nig.ac.jp/rdf/js/jquery-3.5.1.min.js';
+import * as jquery from 'https://rcshige3.nig.ac.jp/rdf/js/jquery-3.5.1.min.js';
 import * as dataTables from 'https://rcshige3.nig.ac.jp/rdf/js/jquery.dataTables.min.js';
 import config from '@/config/config.js';
 
